1694e98c5bb465ec9ace737f5b70983119aea88b
Ozan Tokatli <otokatli@users.noreply.github.com>
2022-07-14 12:59:28 +0100

    added position control code

--- compile.m
+++ compile.m
@@ -2,3 +2,4 @@ cppCompilerConfig = mex.getCompilerConfigurations('C++');
 pathToCompilerDirectory = cppCompilerConfig.Location;
 
 mex sfun_hmInitialize.cpp -lHapticAPI2
+mex sfun_positionControl.cpp -lHapticAPI2
---
+++ sfun_positionControl.cpp
@@ -0,0 +1,259 @@
+/*
+ * sfuntmpl_basic.c: Basic 'C' template for a level 2 S-function.
+ *
+ * Copyright 1990-2018 The MathWorks, Inc.
+ */
+
+
+/*
+ * You must specify the S_FUNCTION_NAME as the name of your S-function
+ * (i.e. replace sfuntmpl_basic with the name of your S-function).
+ */
+
+#define S_FUNCTION_NAME  sfun_positionControl
+#define S_FUNCTION_LEVEL 2
+
+/*
+ * Need to include simstruc.h for the definition of the SimStruct and
+ * its associated macro definitions.
+ */
+#include "simstruc.h"
+#include "HapticAPI2.h"
+#include "HapticMASTER.h"
+
+#define IP_ADDRESS "192.168.0.25"
+long hapticMasterDeviceHandle = 0;
+char response[300];
+
+/* Error handling
+ * --------------
+ *
+ * You should use the following technique to report errors encountered within
+ * an S-function:
+ *
+ *       ssSetErrorStatus(S,"Error encountered due to ...");
+ *       return;
+ *
+ * Note that the 2nd argument to ssSetErrorStatus must be persistent memory.
+ * It cannot be a local variable. For example the following will cause
+ * unpredictable errors:
+ *
+ *      mdlOutputs()
+ *      {
+ *         char msg[256];         {ILLEGAL: to fix use "static char msg[256];"}
+ *         sprintf(msg,"Error due to %s", string);
+ *         ssSetErrorStatus(S,msg);
+ *         return;
+ *      }
+ *
+ */
+
+/*====================*
+ * S-function methods *
+ *====================*/
+
+/* Function: mdlInitializeSizes ===============================================
+ * Abstract:
+ *    The sizes information is used by Simulink to determine the S-function
+ *    block's characteristics (number of inputs, outputs, states, etc.).
+ */
+static void mdlInitializeSizes(SimStruct *S)
+{
+    ssSetNumSFcnParams(S, 0);  /* Number of expected parameters */
+    if (ssGetNumSFcnParams(S) != ssGetSFcnParamsCount(S)) {
+        /* Return if number of expected != number of actual parameters */
+        return;
+    }
+    
+    ssSetNumContStates(S, 0);
+    ssSetNumDiscStates(S, 0);
+    
+    if (!ssSetNumInputPorts(S, 1)) return;
+    ssSetInputPortWidth(S, 0, 3);
+    ssSetInputPortRequiredContiguous(S, 0, true);
+    ssSetInputPortDirectFeedThrough(S, 0, 1);
+    
+    if (!ssSetNumOutputPorts(S, 1)) return;
+    ssSetOutputPortWidth(S, 0, 1);
+    
+    ssSetNumSampleTimes(S, 1);
+    ssSetNumRWork(S, 0);
+    ssSetNumIWork(S, 0);
+    ssSetNumPWork(S, 0);
+    ssSetNumModes(S, 0);
+    ssSetNumNonsampledZCs(S, 0);
+    
+    /* Specify the operating point save/restore compliance to be same as a
+     * built-in block */
+//     ssSetOperatingPointCompliance(S, USE_DEFAULT_OPERATING_POINT);
+    
+//     ssSetRuntimeThreadSafetyCompliance(S, RUNTIME_THREAD_SAFETY_COMPLIANCE_TRUE);
+    ssSetOptions(S, SS_OPTION_EXCEPTION_FREE_CODE);
+}
+
+
+
+/* Function: mdlInitializeSampleTimes =========================================
+ * Abstract:
+ *    This function is used to specify the sample time(s) for your
+ *    S-function. You must register the same number of sample times as
+ *    specified in ssSetNumSampleTimes.
+ */
+static void mdlInitializeSampleTimes(SimStruct *S)
+{
+    ssSetSampleTime(S, 0, INHERITED_SAMPLE_TIME);
+    ssSetOffsetTime(S, 0, 0.0);
+    
+}
+
+
+
+#undef MDL_INITIALIZE_CONDITIONS   /* Change to #undef to remove function */
+#if defined(MDL_INITIALIZE_CONDITIONS)
+/* Function: mdlInitializeConditions ========================================
+ * Abstract:
+ *    In this function, you should initialize the continuous and discrete
+ *    states for your S-function block.  The initial states are placed
+ *    in the state vector, ssGetContStates(S) or ssGetRealDiscStates(S).
+ *    You can also perform any other initialization activities that your
+ *    S-function may require. Note, this routine will be called at the
+ *    start of simulation and if it is present in an enabled subsystem
+ *    configured to reset states, it will be call when the enabled subsystem
+ *    restarts execution to reset the states.
+ */
+static void mdlInitializeConditions(SimStruct *S)
+{
+}
+#endif /* MDL_INITIALIZE_CONDITIONS */
+
+
+
+#define MDL_START  /* Change to #undef to remove function */
+#if defined(MDL_START)
+/* Function: mdlStart =======================================================
+ * Abstract:
+ *    This function is called once at start of model execution. If you
+ *    have states that should be initialized once, this is the place
+ *    to do it.
+ */
+static void mdlStart(SimStruct *S)
+{
+    // Open device
+    hapticMasterDeviceHandle = haDeviceOpen(IP_ADDRESS);
+    
+    // Initialize HapticMaster
+    InitializeDevice(hapticMasterDeviceHandle);
+    
+    // Spring parameters
+    const char *springNameX = "mySpringX";
+    const char *springNameY = "mySpringY";
+    const char *springNameZ = "mySpringZ";
+    const double pos[3] = {0.0, 0.0, 0.0};
+    const double dirX[3] = {1.0, 0.0, 0.0};
+    const double dirY[3] = {0.0, 1.0, 0.0};
+    const double dirZ[3] = {0.0, 0.0, 1.0};
+    const double springConst = 50;
+    const double dampFactor = 0.6;
+    const double maxForce = 25.0;
+    
+    // x-direction
+    createSpring(hapticMasterDeviceHandle,  springNameX, pos, dirX,  springConst, dampFactor, maxForce);
+    
+    // y-direction
+    createSpring(hapticMasterDeviceHandle,  springNameY, pos, dirY,  springConst, dampFactor, maxForce);
+    
+    // z-direction
+    createSpring(hapticMasterDeviceHandle,  springNameZ, pos, dirZ,  springConst, dampFactor, maxForce);
+    
+    // Set device into force mode
+    haSendCommand(hapticMasterDeviceHandle, "set state force", response);
+}
+#endif /*  MDL_START */
+
+
+
+/* Function: mdlOutputs =======================================================
+ * Abstract:
+ *    In this function, you compute the outputs of your S-function
+ *    block.
+ */
+static void mdlOutputs(SimStruct *S, int_T tid)
+{
+    const real_T  *x       = (const real_T*) ssGetInputPortSignal(S, 0);
+    real_T        *y         = ssGetOutputPortRealSignal(S, 0);
+    
+    // Spring names
+    const char *springNameX = "mySpringX";
+    const char *springNameY = "mySpringY";
+    const char *springNameZ = "mySpringZ";
+    
+    // Set new position of x-direction spring
+    setSpringLocation(hapticMasterDeviceHandle, springNameX, &x);
+
+    // Set new position of y-direction spring
+    setSpringLocation(hapticMasterDeviceHandle, springNameY, &x);
+
+    // Set new position of z-direction spring
+    setSpringLocation(hapticMasterDeviceHandle, springNameZ, &x);
+
+    
+    
+    y[0] =  static_cast<real_T>(hapticMasterDeviceHandle);
+}
+
+
+
+#undef MDL_UPDATE  /* Change to #undef to remove function */
+#if defined(MDL_UPDATE)
+/* Function: mdlUpdate ======================================================
+ * Abstract:
+ *    This function is called once for every major integration time step.
+ *    Discrete states are typically updated here, but this function is useful
+ *    for performing any tasks that should only take place once per
+ *    integration step.
+ */
+static void mdlUpdate(SimStruct *S, int_T tid)
+{
+}
+#endif /* MDL_UPDATE */
+
+
+
+#undef MDL_DERIVATIVES  /* Change to #undef to remove function */
+#if defined(MDL_DERIVATIVES)
+/* Function: mdlDerivatives =================================================
+ * Abstract:
+ *    In this function, you compute the S-function block's derivatives.
+ *    The derivatives are placed in the derivative vector, ssGetdX(S).
+ */
+static void mdlDerivatives(SimStruct *S)
+{
+}
+#endif /* MDL_DERIVATIVES */
+
+
+
+/* Function: mdlTerminate =====================================================
+ * Abstract:
+ *    In this function, you should perform any actions that are necessary
+ *    at the termination of a simulation.  For example, if memory was
+ *    allocated in mdlStart, this is the place to free it.
+ */
+static void mdlTerminate(SimStruct *S)
+{
+    haSendCommand(hapticMasterDeviceHandle, "remove all", response);
+    haSendCommand(hapticMasterDeviceHandle, "set state stop", response);
+    
+    haDeviceClose(hapticMasterDeviceHandle);
+}
+
+
+/*=============================*
+ * Required S-function trailer *
+ *=============================*/
+
+#ifdef  MATLAB_MEX_FILE    /* Is this file being compiled as a MEX-file? */
+#include "simulink.c"      /* MEX-file interface mechanism */
+#else
+#include "cg_sfun.h"       /* Code generation registration function */
+#endif
